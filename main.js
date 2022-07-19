3b49b4d8043c61cc2152c9ddb48566307862e30e
Kaynooo <kevyn.fyleyssant@gmail.com>
2022-07-19 11:38:32 +0200

    feat: :zap: watching sub-folders for scss

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ import chalk from "chalk";
 const folder = `./public/scss`;
 let lastUpdate = 0;
 
+const mainFiles = [];
+
 const main = async () => {
   liveServer.start({
     port: 8080,
@@ -21,51 +23,56 @@ const main = async () => {
     open: false,
   });
 
-  fs.watch(folder, (event, filename) => {
-    compileStyle(folder, "main.scss");
-  });
-
-  fs.readdirSync(folder).forEach((filename) => {
-    compileStyle(folder, "main.scss");
-  });
+  watch("./public/scss");
 };
 
 main();
 
-function compileStyle(folder, filename) {
+function watch(folder) {
+  fs.readdirSync(folder).forEach((filename) => {
+    if (fs.statSync(path.join(folder, filename)).isDirectory()) {
+      watch(path.join(folder, filename));
+    } else {
+      if (filename.endsWith(".scss") && !filename.startsWith("_")) {
+        mainFiles.push(path.join(folder, filename));
+      }
+
+      compileStyle(folder);
+    }
+  });
+
+  fs.watch(folder, (_event, filename) => {
+    if (filename.endsWith(".scss") && !filename.startsWith("_")) {
+      mainFiles.push(path.join(folder, filename));
+    }
+
+    compileStyle(folder);
+  });
+}
+
+function compileStyle(folder) {
   if (lastUpdate > Date.now() - 100) return;
   lastUpdate = Date.now();
 
-  if (filename && filename.endsWith(".scss") && !filename.startsWith("_")) {
+  for (const filename of mainFiles) {
     try {
-      const result = sass.compile(path.join(folder, filename), {
+      const result = sass.compile(filename, {
         style: "compressed",
         sourceMap: true,
       });
 
-      fs.writeFileSync(
-        path.join(
-          folder.replace("scss", "css"),
-          filename.replace(".scss", ".css")
-        ),
-        result.css
-      );
+      fs.writeFileSync(filename.replace(/scss/g, "css"), result.css);
 
       fs.writeFileSync(
-        path.join(
-          folder.replace("scss", "css"),
-          filename.replace(".scss", ".map.css")
-        ),
+        filename.replace(/scss/g, "css").replace(".css", ".map.css"),
         JSON.stringify(result.sourceMap)
       );
 
-      console.log(`Compiled ${path.join(folder, filename)}`);
+      console.log(`Compiled ${filename}`);
     } catch (e) {
       console.error(
         chalk.red(
-          `Error while compiling ${path.join(folder, filename)}\n${
-            String(e).split("public")[0]
-          }`
+          `Error while compiling ${filename}\n${String(e).split("public")[0]}`
         )
       );
     }
